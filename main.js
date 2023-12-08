de2a501d800a2e1131d7f10e8a8ff62a5e22e1e0
Gautam <gautamanand931@gmail.com>
2023-12-08 22:36:51 +0530

    testimonials transition added

--- main.js
+++ main.js
@@ -13,23 +13,25 @@ for (let i = 0; i < cards.length; i++) {
 }
 
 let indicator = document.querySelectorAll(".indicator");
+
 function nextcard() {
-  cards[curcard].style.display = "none";
+  cards[curcard].style.opacity = 0;
   indicator[curcard].style.backgroundColor = "grey";
   curcard = curcard == cards.length - 1 ? 0 : curcard + 1;
-  cards[curcard].style.display = "block";
+  cards[curcard].style.opacity = 1;
   indicator[curcard].style.backgroundColor = "black";
 }
 function previouscard() {
-  cards[curcard].style.display = "none";
+  cards[curcard].style.opacity = 0;
   indicator[curcard].style.backgroundColor = "grey";
   curcard = curcard == 0 ? cards.length - 1 : curcard - 1;
-  cards[curcard].style.display = "block";
+  cards[curcard].style.opacity = 1;
   indicator[curcard].style.backgroundColor = "black";
 }
 
 next.addEventListener("click", nextcard);
 previous.addEventListener("click", previouscard);
+setInterval(function(){nextcard()},2000);
 
 // Navbar
 document.addEventListener("DOMContentLoaded", function () {
